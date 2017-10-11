fcd8cc2dd29d53f4c84638d65215fdd7ef1db7f1
Rory Johnson <redkjohnson@gmail.com>
2017-10-11 16:49:51 -0400

    account balance is now updated via the bank api

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import AccountBalance from './components/AccountBalance';
+import Debits from './components/Debits';
+import Credits from './components/Credits';
+
+import axios from 'axios';
 
 class App extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      accountBalance: 14568.27,
-      currentUser: {
-        userName: 'bob_loblaw',
-        memberSince: '08/23/99'
-      }
-    }
+  state = {
+    user: {
+      userName: 'Bob',
+      memberSince: 1950
+    },
+    accountBalance: 12345.34 //credits - debits
   }
 
-  render() {
-
-    const HomeComponent = () =>{return <Home accountBalance={this.state.accountBalance}/>};
-    const AccountBalanceWrapper = () =>{
-      return <AccountBalance accountBalance={this.state.accountBalance}/>
+  
+  render () {
+    const getBalance = async () =>{
+      let debits = await axios.get("http://localhost:4000/debits")
+      let credits = await axios.get('http://localhost:4000/credits');
+      if(debits.status === 200 && credits.status === 200){
+        let debitTotal = debits.data.map((data)=>{return data.amount}).reduce((sum, val)=>{return sum+val});
+        let creditTotal = credits.data.map((data)=>{return data.amount}).reduce((sum, val)=>{return sum+val});
+        this.setState({accountBalance: creditTotal-debitTotal});
+      }else{
+        console.log('Error!');
+      }
     }
-    const UserProfileComponent = () =>{
-      return <UserProfile
-        userName={this.state.currentUser.userName}
-        memberSince={this.state.currentUser.memberSince}
-      />
+
+    const AccountBalanceWrapper = () => {
+      return (<AccountBalance
+        accountBalance={this.state.accountBalance}
+        getBalance={getBalance}
+      />)
     }
     
+    const UserProfileWrapper = () => {
+      return (<UserProfile
+        userName={this.state.user.userName}
+        memberSince={this.state.user.memberSince}
+      />)
+    }
+
+    const DebitsWrapper = () =>{
+      return(<Debits/>)
+    }
+
+    const CreditsWrapper = () =>{
+      return(<Credits/>)
+    }
+
     return (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomeComponent}/>
-            <Route exact path="/account" component={AccountBalanceWrapper}/>
-            <Route exact path="/userProfile" component={UserProfileComponent}/>
-          </Switch>
-        </Router>
-    );
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/account" render={AccountBalanceWrapper}/>
+          <Route exact path="/debits" render={DebitsWrapper}/>
+          {<Route exact path="/credits" render={CreditsWrapper}/>}
+          <Route exact path="/user" render={UserProfileWrapper} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
